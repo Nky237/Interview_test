8310d62dd07af347c0462e589b6eca6841616f79
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-08-17 00:54:42 +0100

    made some more chamges

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -1,19 +1,23 @@
-import React from 'react'
-import Image from 'next/image'
-import Logo from '../assets/logo.webp'
+import React from "react";
+import Image from "next/image";
+import Logo from "../assets/logo.webp";
+import { GoPersonAdd } from "react-icons/go";
 
 const Nav = () => {
   return (
-    <div className='Navy'>
-        <div className = 'Nav_Image'>
-            <Image src={Logo} alt = 'logo' height= {50} width = {50} />
-        </div>
-        <div className = 'Nav_Button'>
-            <button className='colored'>Register</button>
-            <button className='non_colored'>Login</button>
-        </div>
+    <div className="Navy">
+      <div className="Nav_Image">
+        <Image src={Logo} alt="logo" height={50} width={50} />
+      </div>
+      <div className="Nav_Button">
+        <button className="colored">
+          <GoPersonAdd className='icon' />
+          Register
+        </button>
+        <button className="non_colored">Login</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
