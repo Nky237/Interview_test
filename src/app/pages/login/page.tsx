c9554e2df7b09c00bf6a342b235a2d671861ad6b
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-08-16 15:53:50 +0100

    added more feature

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -1,13 +1,37 @@
-import React from 'react'
-import Link from 'next/link'
-
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import LoginImage from "../../../assets/signIn_img.webp";
 const Login = () => {
   return (
-    <div>
-      <h1>Login </h1>
+    <div className="Login_Page">
+      <div className="Loginy">
+        <div className="Logintext">
+          <h1>We've Missed You</h1>
+          <p>
+            Kindly signin and get first access to the very best, community and
+            unlock more opportunities.
+          </p>
+          <form action="">
+            <div>
+              <label>Email</label>
+              <input type="email" />
+            </div>
+            <div>
+              <label>Password</label>
+              <input type="password" />
+            </div>
+            <button className="colored">Login</button> <br />
+            <Link href="#">Forgot username or password?</Link>
+          </form>
+        </div>
+        <div className="Loginimage">
+          <Image src={LoginImage} alt="loginImage" height={100} width={100} />
+        </div>
+      </div>
       <Link href="/">Home</Link>
     </div>
   );
-}
+};
 
-export default Login
+export default Login;
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
+import Logo from '../assets/logo.webp'
 
 const Nav = () => {
   return (
-    <div>
+    <div className='Navy'>
         <div className = 'Nav_Image'>
-            <Image src={}alt = 'logo' height= {50} width = {50} />
+            <Image src={Logo}alt = 'logo' height= {50} width = {50} />
         </div>
         <div className = 'Nav_Button'>
-
+            <button className='colored'>Register</button>
+            <button className='non_colored'>Login</button>
         </div>
     </div>
   )
