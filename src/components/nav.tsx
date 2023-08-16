26db7f7ad89b816892bcaaaafa71f7cbad2eaba0
Ezike Nkemdili Jane <ezikenkemdilijane@gmail.com>
2023-08-16 15:04:26 +0100

    first commit

---
+++ src/components/nav.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Nav = () => {
+  return (
+    <div>
+        <div className = 'Nav_Image'>
+            <Image src={}alt = 'logo' height= {50} width = {50} />
+        </div>
+        <div className = 'Nav_Button'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Nav
